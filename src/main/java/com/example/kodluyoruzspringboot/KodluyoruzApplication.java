1bb22eb6e18d06e4040e8cc26bd3404bd36b451b
sedatalagz <108261597+sedatalagz@users.noreply.github.com>
2022-07-06 10:45:36 +0300

    first commit (Spring)

---
+++ src/main/java/com/example/kodluyoruzspringboot/KodluyoruzApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodluyoruzspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodluyoruzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kodluyoruzspringboot/KodluyoruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodluyoruzspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
