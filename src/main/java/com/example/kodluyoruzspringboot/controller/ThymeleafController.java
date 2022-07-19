deea6d1ce0a0b9243710801cb94daaae71800a50
sedatalagz <108261597+sedatalagz@users.noreply.github.com>
2022-07-19 15:21:27 +0300

    @RequestParam

--- src/main/java/com/example/kodluyoruzspringboot/controller/ThymeleafController.java
+++ src/main/java/com/example/kodluyoruzspringboot/controller/ThymeleafController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -83,7 +84,9 @@ public class ThymeleafController {
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //@PathVariable
     // http://localhost:8080/thymeleaf6/4
+    // http://localhost:8080/thymeleaf6/
     @GetMapping({"/thymeleaf6","/thymeleaf6/{id}"}) //url yapısı
     public String getThymeleaf6Object(Model model, @PathVariable(name ="id", required = false) Long id) {
         if (id!=null){
@@ -95,5 +98,22 @@ public class ThymeleafController {
 
 
         return "thymeleaf6";
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //@RequestParam
+    // http://localhost:8080/thymeleaf7?id=4&name=SedatALAGOZ
+    @GetMapping("/thymeleaf7") //url yapısı
+    public String getThymeleaf7Object(Model model,
+                                      @RequestParam(name ="id", required = false, defaultValue = "0") Long id,
+                                      @RequestParam(name ="name") String adi
+    ) {
+        if (id!=null){
+            model.addAttribute("key_model1", " id: " + id + " Adı Soyadı " + adi);
+        }
+        else{
+            model.addAttribute("key_model1", "id bulunamadı ");
+        }
+        return "thymeleaf7";
     }
 }
