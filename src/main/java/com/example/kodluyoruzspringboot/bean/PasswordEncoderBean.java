da1c5adca27a4255b466fa8e5eb2be770bfcb7c4
sedatalagz <108261597+sedatalagz@users.noreply.github.com>
2022-07-26 15:00:48 +0300

    ModelMapperBean (inactive)

--- src/main/java/com/example/kodluyoruzspringboot/bean/PasswordEncoderBean.java
+++ src/main/java/com/example/kodluyoruzspringboot/bean/PasswordEncoderBean.java
@@ -1,14 +1,10 @@
 package com.example.kodluyoruzspringboot.bean;
-
-
 /*import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 @Configuration*/
 public class PasswordEncoderBean {
-
   /*  @Bean
     public PasswordEncoder passwordEncoderBean(){
         return new BCryptPasswordEncoder();
